3f9b09169d826720cd58c777d5fbb5496ab4c85e
Krystian Nowak <krystian.k.nowak@gmail.com>
2024-08-08 15:38:51 +0200

    basic db repository

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
-@Module({})
+@Module({
+  imports: [MongooseModule.forRoot('monogodb://127.0.0.1/sleepr')],
+})
 export class DatabaseModule {}
